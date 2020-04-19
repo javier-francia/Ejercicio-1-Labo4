bcf2d48f73949d9d88051c327a5f17682553426f
Javier Francia Peñarrocha <javifrancia@gmail.com>
2020-04-19 04:03:00 -0300

    Punto 1 con firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD3EgZOBre77__-t18j92o6oqQt9pNUx6E",
+    authDomain: "ej1-labo4-javier-francia-utn.firebaseapp.com",
+    databaseURL: "https://ej1-labo4-javier-francia-utn.firebaseio.com",
+    projectId: "ej1-labo4-javier-francia-utn",
+    storageBucket: "ej1-labo4-javier-francia-utn.appspot.com",
+    messagingSenderId: "539662597551",
+    appId: "1:539662597551:web:d3d276323c325096ff835a"
+  }
 };
